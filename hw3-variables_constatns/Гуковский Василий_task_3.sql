cfcbf3a97f42f246625f49529e5d361d6a414c9b
Василий Гуковский <vasily.gukovsky2017@yandex.ru>
2025-04-23 07:57:45 +0300

    ДЗ 3: Переменные и константы. Внес изменения

--- hw3-variables_constatns/Гуковский Василий_task_3.sql
+++ hw3-variables_constatns/Гуковский Василий_task_3.sql
@@ -2,52 +2,51 @@
 Описание скрипта: Демонстрация анонимных блоков.  
 Вывод сообщений в буфер вывода с использованием процедуры: dbms_output.put_line().*/
 
-declare
-c_create number(1) := 0;
-c_success number(1) := 1;
-c_reset number(1) := 2;
-c_cancel number(1) := 3;
-
-v_balance varchar2(50) := 'недостаточно средств'; 
-v_error varchar2(50) := 'ошибка пользователя';
-
-v_add_payments varchar2(120) := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
-v_del_payments varchar2(100) := 'Детали платежа удалены по списку id_полей';
-begin
 -- Создание платежа
+declare
+   c_created constant number(1) := 0;
+   v_message varchar2(50) := 'Платеж создан';
 begin
-  dbms_output.put_line('Платеж создан. Статус: '|| c_create);
-end;
-
--- Сброс платежа в "ошибочный статус"
-begin
-  dbms_output.put_line('Сброс платежа в "ошибочный статус" с указанием причины. Статус: '|| c_reset ||'. Причина: '|| v_balance);
-end;
-
--- Отмена платежа с указанием причины
-begin
-  dbms_output.put_line('Отмена платежа с указанием причины. Статус: '|| c_cancel ||'. Причина: ' || v_error);
-end;
-
--- Успешное завершение платежа
-begin
-  dbms_output.put_line('Успешное завершение платежа. Статус: '|| c_success);
-end;
-
--- Добавление или обновление платежа
-begin
-  dbms_output.put_line(v_add_payments);
-end;
-
--- Удаление деталей платежа
-begin
-  dbms_output.put_line(v_del_payments);
-end;
-
+  dbms_output.put_line(v_message ||'. Статус: '|| c_created);
 end;
 /
-
-
-
-
-
+-- Сброс платежа в "ошибочный статус"
+declare
+   c_error constant number(1) := 2; 
+   v_reason varchar2(50) := 'недостаточно средств'; 
+   v_message varchar2(130) := 'Сброс платежа в "ошибочный статус" с указанием причины. Статус: '; 
+begin
+  dbms_output.put_line(v_message || c_error ||'. Причина: '|| v_reason);
+end;
+/
+-- Отмена платежа с указанием причины
+declare
+   c_cancel constant number(1) := 3;
+   v_reason varchar2(100) := 'Причина: ошибка пользователя';
+   v_message varchar2(100) := 'Отмена платежа с указанием причины. Статус: ';
+begin
+  dbms_output.put_line(v_message || c_cancel ||'. '|| v_reason);
+end;
+/
+-- Успешное завершение платежа
+declare
+   c_success constant number(1) := 1;
+   v_message varchar2(100) := 'Успешное завершение платежа. Статус: ';
+begin
+  dbms_output.put_line(v_message || c_success);
+end;
+/
+-- Добавление или обновление платежа
+declare
+   v_message varchar2(130) := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
+begin
+  dbms_output.put_line(v_message);
+end;
+/
+-- Удаление деталей платежа
+declare
+   v_message varchar2(130) := 'Детали платежа удалены по списку id_полей';
+begin
+  dbms_output.put_line(v_message);
+end;
+/
