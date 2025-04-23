704f94d31992c086a9b05fdd24c501fc9a9e2f27
expert2013 <36111564+expert2013@users.noreply.github.com>
2025-04-23 22:04:25 +0300

    Hw4 (#6)
    * ДЗ 3: Переменные и константы
    
    * ДЗ 3: Переменные и константы. Внес изменения
    
    * ДЗ 4: Типы данных
    
    * ДЗ 4: Типы данных. (Переделал вывод переменных)

---
+++ hw4-data_types/Гуковский Василий_task_4.sql
@@ -0,0 +1,64 @@
+/*Автор: Гуковский Василий
+Описание скрипта: Демонстрация анонимных блоков.  
+Вывод сообщений в буфер вывода с использованием процедуры: dbms_output.put_line().*/
+
+-- Создание платежа
+declare
+   c_created constant number(10) := 0;
+   v_message varchar2(100 char) := 'Платеж создан';
+   v_current_date    date := sysdate;
+begin
+  dbms_output.put_line(v_message ||'. Статус: '|| c_created);
+  dbms_output.put_line(to_char(v_current_date, 'yyyy-mm-dd'));
+end;
+/
+-- Сброс платежа в "ошибочный статус"
+declare
+   c_error constant number(10) := 2; 
+   v_reason varchar2(50 char) := 'недостаточно средств'; 
+   v_message varchar2(100 char) := 'Сброс платежа в "ошибочный статус" с указанием причины. Статус: '; 
+   v_current_date    date := sysdate;
+begin
+  dbms_output.put_line(v_message || c_error ||'. Причина: '|| v_reason);
+  dbms_output.put_line(to_char(v_current_date, 'yyyy-mm-dd hh24:mi:ss'));
+end;
+/
+-- Отмена платежа с указанием причины
+declare
+   c_cancel constant number(10) := 3;
+   v_reason varchar2(50 char) := 'Причина: ошибка пользователя';
+   v_message varchar2(100 char) := 'Отмена платежа с указанием причины. Статус: ';
+   v_current_date    date := sysdate;
+begin
+  dbms_output.put_line(v_message || c_cancel ||'. '|| v_reason);
+  dbms_output.put_line(to_char(v_current_date, 'dd/mon/yy hh24:mi:ss'));
+end;
+/
+-- Успешное завершение платежа
+declare
+   c_success           constant number(10) := 1;
+   v_message           varchar2(100 char) := 'Успешное завершение платежа. Статус: ';
+   v_current_timestamp timestamp := systimestamp;
+begin
+  dbms_output.put_line(v_message || c_success);
+  dbms_output.put_line(to_char(v_current_timestamp, 'dd/mm/yy hh24:mi:ss.sssss'));
+end;
+/
+-- Добавление или обновление платежа
+declare
+   v_message varchar2(100 char) := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
+   v_current_timestamp timestamp := systimestamp;
+begin
+  dbms_output.put_line(v_message);
+  dbms_output.put_line(to_char(v_current_timestamp,'yyyy-mm-dd hh24: mi: ss.ff'));
+end;
+/
+-- Удаление деталей платежа
+declare
+   v_message varchar2(100 char) := 'Детали платежа удалены по списку id_полей';
+   v_current_timestamp timestamp := systimestamp;
+begin
+  dbms_output.put_line(v_message);
+  dbms_output.put_line(to_char(v_current_timestamp,'dd-mm-rr hh.mi.ss.ff am'));
+end;
+/
